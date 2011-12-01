8bd5b4439be34922363b54cacf82cbe8d5e509b1
Kyle Evans <kevans@android-x86.org>
2011-12-01 09:07:40 -0500

    Add new .idc files for input devices

--- tx2500.mk
+++ tx2500.mk
@@ -9,4 +9,8 @@ PRODUCT_MANUFACTURER := HP
 
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlays
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/init.sh:system/etc/init.sh
+	$(LOCAL_PATH)/init.sh:system/etc/init.sh \
+	$(LOCAL_PATH)/input/Wacom_ISDv4_93_Pen.idc:system/usr/idc/Wacom_ISDv4_93_Pen.idc \
+	$(LOCAL_PATH)/input/Wacom_ISDv4_93_Finger.idc:system/usr/idc/Wacom_ISDv4_93_Finger.idc \
+	$(LOCAL_PATH)/input/SynPS_2_Synaptics_TouchPad.idc:system/usr/idc/SynPS_2_Synaptics_TouchPad.idc
+
