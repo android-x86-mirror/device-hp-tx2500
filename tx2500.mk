2b11173cf61a2a41f29a03ff6a11b4f2571f18e5
Kyle Evans <kevans@android-x86.org>
2012-02-18 15:24:56 -0500

    Add a hack to read bezel buttons manually
    Untill a proper fix is found for reading these buttons, this patch reprograms the wireless switch for
    manually calling the hotkey query. The included dsdt is unreferenced and must be added manually at build time
    by users wishing to include it.

--- tx2500.mk
+++ tx2500.mk
@@ -10,7 +10,7 @@ DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlays
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/init.sh:system/etc/init.sh \
 	$(LOCAL_PATH)/input/AT_Translated_Set_2_keyboard.kl:system/usr/keylayout/AT_Translated_Set_2_keyboard.kl \
+	$(LOCAL_PATH)/input/HP_WMI_hotkeys.kl:system/usr/keylayout/HP_WMI_hotkeys.kl \
 	$(LOCAL_PATH)/input/Wacom_ISDv4_93_Pen.idc:system/usr/idc/Wacom_ISDv4_93_Pen.idc \
 	$(LOCAL_PATH)/input/Wacom_ISDv4_93_Finger.idc:system/usr/idc/Wacom_ISDv4_93_Finger.idc \
 	$(LOCAL_PATH)/input/SynPS_2_Synaptics_TouchPad.idc:system/usr/idc/SynPS_2_Synaptics_TouchPad.idc
-
