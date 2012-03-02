7a0964ee538b613894b818a9321daa105d155378
Kyle Evans <kevans@android-x86.org>
2012-03-02 11:17:18 -0500

    Update for ics

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 LOCAL_FIRMWARES := $(subst $(LOCAL_PATH)/../../common/firmware/,,$(shell find $(LOCAL_PATH)/../../common/firmware/radeon -type f)) 
 
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/tx2500_info
-TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/tx2500_minimal
+TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/tx2500_defconfig
 
 include $(GENERIC_X86_ANDROID_MK)
 
--- tx2500.mk
+++ tx2500.mk
@@ -6,11 +6,9 @@ PRODUCT_NAME := tx2500
 PRODUCT_DEVICE := tx2500
 PRODUCT_MANUFACTURER := HP
 
-DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlays
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlays
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/init.sh:system/etc/init.sh \
+	$(LOCAL_PATH)/tx2500.sh:system/etc/tx2500.sh \
 	$(LOCAL_PATH)/input/AT_Translated_Set_2_keyboard.kl:system/usr/keylayout/AT_Translated_Set_2_keyboard.kl \
 	$(LOCAL_PATH)/input/HP_WMI_hotkeys.kl:system/usr/keylayout/HP_WMI_hotkeys.kl \
-	$(LOCAL_PATH)/input/Wacom_ISDv4_93_Pen.idc:system/usr/idc/Wacom_ISDv4_93_Pen.idc \
-	$(LOCAL_PATH)/input/Wacom_ISDv4_93_Finger.idc:system/usr/idc/Wacom_ISDv4_93_Finger.idc \
 	$(LOCAL_PATH)/input/SynPS_2_Synaptics_TouchPad.idc:system/usr/idc/SynPS_2_Synaptics_TouchPad.idc
