ccc963f6b447982a4d7a4efd2dc15833d9715186
Kyle Evans <kevans@android-x86.org>
2012-03-02 10:13:58 -0500

    Enable internal + usb + card reader storage

--- tx2500.mk
+++ tx2500.mk
@@ -10,6 +10,7 @@ PRODUCT_MANUFACTURER := HP
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlays
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/tx2500.sh:system/etc/tx2500.sh \
+	$(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
 	$(LOCAL_PATH)/input/AT_Translated_Set_2_keyboard.kl:system/usr/keylayout/AT_Translated_Set_2_keyboard.kl \
 	$(LOCAL_PATH)/input/HP_WMI_hotkeys.kl:system/usr/keylayout/HP_WMI_hotkeys.kl \
 	$(LOCAL_PATH)/input/SynPS_2_Synaptics_TouchPad.idc:system/usr/idc/SynPS_2_Synaptics_TouchPad.idc
